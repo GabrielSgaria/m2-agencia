2d5cbaa6dc91fcaa353e88d4e792c3f04d8452a5
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-03-19 12:27:22 -0300

    Accordion 100%

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,20 @@ import { AccordionRadix } from "@/components/accordion-radix";
 import { NavBar } from "@/components/nav-bar";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 
-
 export default function Home() {
   return (
     <div>
       <NavBar />
       <section className="bg-neutral-950">
         <div className="container mx-auto flex flex-col items-center justify-center relative z-10 h-[650px]">
-          <div className="flex w-full flex-col items-center justify-center gap-8 sm:flex-row sm:justify-between">
+          <div className="flex w-full flex-col items-center justify-center gap-8 ">
             <div className="flex flex-col items-center justify-center sm:items-start">
               <p className="bebasNeue text-2xl tracking-[0.2em] text-neutral-500 sm:mb-6 sm:text-3xl">
                 SOBRE NÓS
               </p>
               <div className="text-center text-5xl font-semibold leading-tight text-neutral-100 mt-8 sm:text-start sm:text-8xl">
                 <h1>M2 AGÊNCIA</h1>
-                <span className="font-black"> DE MARKETING DIGITAL</span>
+                <span className="font-black"> DE MARKETING </span>
               </div>
               <p className="saira max-w-[400px] text-center text-xl text-neutral-200 mt-8 sm:max-w-[900px] sm:text-center sm:text-2xl mx-auto">
                 Elevar Marcas com Autenticidade e Inovação Nossa missão é clara
@@ -41,7 +40,6 @@ export default function Home() {
             <div></div>
           </div>
           <AccordionRadix />
-          
         </div>
       </section>
     </div>
--- src/components/accordion-radix.tsx
+++ src/components/accordion-radix.tsx
@@ -1,86 +1,70 @@
 import * as Accordion from "@radix-ui/react-accordion";
 import { ChevronDownIcon } from "@radix-ui/react-icons";
 
-export function AccordionRadix(){
-    return(
-        <div className="flex justify-center items center container mx-auto mt-16">
-            <Accordion.Root
-              className="rounded-md w-3/4 bg-neutral-950 text-neutral-100 shadow-xl text-center"
-              type="single"
-              collapsible
-            >
-              <Accordion.Item value="item-1">
-                <Accordion.Trigger className="uppercase flex flex-row justify-between w-full items-center p-4 border-y rounded-md border-solid border-neutral-800">
-                  <span className="text-2xl font-semibold">
-                    Marketing para Negócios Locais e Delivery
-                  </span>
-                  <ChevronDownIcon
-                    className="AccordionChevron size-8"
-                    aria-hidden
-                  />
-                </Accordion.Trigger>
-                <Accordion.Content className="py-2 px-3 text-lg">
-                  Prestamos serviço para negocios locais e delivery Prestamos
-                  serviço para negocios locais e delivery Prestamos serviço para
-                  negocios locais e delivery Prestamos serviço para negocios
-                  locais e delivery 
-                </Accordion.Content>
-              </Accordion.Item>
+export function AccordionRadix() {
+  return (
+    <div className="flex justify-center items center container mx-auto mt-16">
+      <Accordion.Root
+        className="rounded-md w-full mx-1 sm:w-3/4 bg-neutral-950 text-neutral-100 shadow-xl text-center"
+        type="single"
+        collapsible
+      >
+        <Accordion.Item value="item-1">
+          <Accordion.Trigger className="AccordionTrigger uppercase flex flex-row justify-between w-full items-center p-4 border-y rounded-md border-solid border-neutral-900">
+            <span className="font-black text-xl w-full text-start">
+              Marketing para Negócios Locais e Delivery
+            </span>
+            <ChevronDownIcon className="AccordionChevron size-8" aria-hidden />
+          </Accordion.Trigger>
+          <Accordion.Content className="AccordionContent py-4 px-3 saira text-xl text-neutral-100 bg-neutral-900">
+            Prestamos serviço para negocios locais e delivery Prestamos serviço
+            para negocios locais e delivery Prestamos serviço para negocios
+            locais e delivery Prestamos serviço para negocios locais e delivery
+          </Accordion.Content>
+        </Accordion.Item>
 
-              <Accordion.Item value="item-2">
-                <Accordion.Trigger className="uppercase flex flex-row justify-between w-full items-center  p-4 border-y rounded-md border-solid border-neutral-800">
-                  <span className="text-2xl font-semibold">
-                    GERENCIAMENTO DE REDES SOCIAIS
-                  </span>
-                  <ChevronDownIcon
-                    className="AccordionChevron size-8"
-                    aria-hidden
-                  />
-                </Accordion.Trigger>
-                <Accordion.Content className="py-2 px-3 text-lg">
-                  Prestamos serviço para negocios locais e delivery Prestamos
-                  serviço para negocios locais e delivery Prestamos serviço para
-                  negocios locais e delivery Prestamos serviço para negocios
-                  locais e delivery 
-                </Accordion.Content>
-              </Accordion.Item>
+        <Accordion.Item value="item-2">
+          <Accordion.Trigger className="AccordionTrigger uppercase flex flex-row justify-between w-full items-center  p-4 border-y rounded-md border-solid border-neutral-900">
+            <span className="font-black text-xl w-full text-start">
+              GERENCIAMENTO DE REDES SOCIAIS
+            </span>
+            <ChevronDownIcon className="AccordionChevron size-8" aria-hidden />
+          </Accordion.Trigger>
+          <Accordion.Content className="AccordionContent py-4 px-3 saira text-xl text-neutral-100  bg-neutral-900">
+            Prestamos serviço para negocios locais e delivery Prestamos serviço
+            para negocios locais e delivery Prestamos serviço para negocios
+            locais e delivery Prestamos serviço para negocios locais e delivery
+          </Accordion.Content>
+        </Accordion.Item>
 
-              <Accordion.Item value="item-3">
-                <Accordion.Trigger className="uppercase flex flex-row justify-between w-full items-center  p-4 border-y rounded-md border-solid border-neutral-800">
-                  <span className="text-2xl font-semibold">
-                    SITE PROFISSIONAL
-                  </span>
-                  <ChevronDownIcon
-                    className="AccordionChevron size-8"
-                    aria-hidden
-                  />
-                </Accordion.Trigger>
-                <Accordion.Content className="py-2 px-3 text-lg">
-                  Prestamos serviço para negocios locais e delivery Prestamos
-                  serviço para negocios locais e delivery Prestamos serviço para
-                  negocios locais e delivery Prestamos serviço para negocios
-                  locais e delivery 
-                </Accordion.Content>
-              </Accordion.Item>
+        <Accordion.Item value="item-3">
+          <Accordion.Trigger className="AccordionTrigger uppercase flex flex-row justify-between w-full items-center p-4 border-y rounded-md border-solid border-neutral-900">
+            <span className="font-black text-xl w-full text-start">
+              SITE PROFISSIONAL 
+            </span>
+            <ChevronDownIcon className="AccordionChevron size-8" aria-hidden />
+          </Accordion.Trigger>
+          <Accordion.Content className="AccordionContent py-4 px-3 saira text-xl text-neutral-100  bg-neutral-900">
+            Prestamos serviço para negocios locais e delivery Prestamos serviço
+            para negocios locais e delivery Prestamos serviço para negocios
+            locais e delivery Prestamos serviço para negocios locais e delivery
+          </Accordion.Content>
+        </Accordion.Item>
 
-              <Accordion.Item value="item-4">
-                <Accordion.Trigger className="uppercase flex flex-row justify-between w-full items-center  p-4 border-y rounded-md border-solid border-neutral-800">
-                  <span className="text-2xl font-semibold">
-                    BANNERS, E-MAIL MARKETING E IMPRESSOS
-                  </span>
-                  <ChevronDownIcon
-                    className="AccordionChevron size-8"
-                    aria-hidden
-                  />
-                </Accordion.Trigger>
-                <Accordion.Content className="py-2 px-3 text-lg">
-                  Prestamos serviço para negocios locais e delivery Prestamos
-                  serviço para negocios locais e delivery Prestamos serviço para
-                  negocios locais e delivery Prestamos serviço para negocios
-                  locais e delivery 
-                </Accordion.Content>
-              </Accordion.Item>
-            </Accordion.Root>
-          </div>
-    )
+        <Accordion.Item value="item-4">
+          <Accordion.Trigger className="AccordionTrigger uppercase flex flex-row justify-between w-full items-center p-4 border-y rounded-md border-solid border-neutral-900">
+            <span className="font-black text-xl w-full text-start">
+              BANNERS, E-MAIL MARKETING E IMPRESSOS
+            </span>
+            <ChevronDownIcon className="AccordionChevron size-8" aria-hidden />
+          </Accordion.Trigger>
+          <Accordion.Content className="AccordionContent py-4 px-3 saira text-xl text-neutral-100 rounded-md bg-neutral-900">
+            Prestamos serviço para negocios locais e delivery Prestamos serviço
+            para negocios locais e delivery Prestamos serviço para negocios
+            locais e delivery Prestamos serviço para negocios locais e delivery
+          </Accordion.Content>
+        </Accordion.Item>
+      </Accordion.Root>
+    </div>
+  );
 }
